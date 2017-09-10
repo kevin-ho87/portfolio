021b3e64bec46a4243ad7ffb1ec43a873592b966
kevin-ho87 <31581926+kevin-ho87@users.noreply.github.com>
2017-09-10 22:41:31 +1000

    Home projects
    Move home projects to component

---
+++ components/HomeProjects.vue
@@ -0,0 +1,92 @@
+<template>
+  <div id="home-projects" class="projects" @mousemove="projectsOver" @mouseleave="projectsLeave">
+    <a href="#" class="project project_nintendo">
+      <img src="~assets/img/logo-nintendo.png" alt="">
+    </a>
+    <a href="#" class="project project_mind">
+      <img src="~assets/img/logo-mind.png" alt="">
+    </a>
+    <a href="#" class="project project_cav">
+      <img src="~assets/img/logo-cav.png" alt="">
+    </a>
+    <a href="#" class="project project_telstra">
+      <img src="~assets/img/logo-telstra.png" alt="">
+    </a>
+  </div>
+</template>
+
+<script>
+import { TweenLite, Back } from 'gsap'
+
+export default {
+  mounted () {
+    TweenLite.set('#home-projects', {
+      rotationZ: '-0.1',
+      transformPerspective: 500,
+      transformOrigin: 'center center'
+    })
+  },
+  methods: {
+    projectsOver (e) {
+      const projectRect = document.getElementById('home-projects').getBoundingClientRect()
+
+      let xPosTilt = e.layerX / projectRect.width * 100 - 50
+      let yPosTilt = e.layerY / projectRect.height * 100 - 50
+
+      TweenLite.to('#home-projects', 0.7, {
+        rotationY: 0.05 * xPosTilt,
+        rotationX: 0.20 * yPosTilt
+      })
+    },
+    projectsLeave () {
+      TweenLite.to('#home-projects', 0.3, {
+        rotation: 0,
+        rotationX: 0,
+        ease: Back.easeOut
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~assets/sass/base/settings";
+
+.projects {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 200px;
+  position: relative;
+  z-index: 2;
+}
+
+.project {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 250px;
+  height: 150px;
+  flex: 1 1 50%;
+  padding: rem-calc(10px);
+
+  &_nintendo {
+    background-color: $c-nintendo;
+  }
+  &_mind {
+    background-color: $c-mind;
+  }
+  &_cav {
+    background-color: $c-cav;
+  }
+  &_telstra {
+    background-color: $c-telstra;
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
+    max-height: 100%;
+  }
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -12,20 +12,7 @@
           <path d="M971.333 730.968c0-83.69 69.846-151.635 155.877-151.635 86.03 0 155.876 67.946 155.876 151.635 0 83.69-69.845 151.635-155.876 151.635-86.03 0-155.877-67.945-155.877-151.635z" fill="#163264"/><path d="M1067.304 535.65c7.394-11.788 17.614-18.713 22.81-15.455 5.193 3.258 3.408 15.473-3.985 27.26-7.395 11.79-17.614 18.715-22.81 15.457-5.194-3.26-3.41-15.474 3.984-27.262z" fill="#FEB931"/><path d="M1095.224 575.31c0-19.57 15.397-35.46 34.362-35.46 18.965 0 34.362 15.89 34.362 35.46 0 19.573-15.397 35.463-34.362 35.463-18.965 0-34.362-15.89-34.362-35.462z" fill="#FFEF02"/><path d="M1063.602 544.972c0-19.572 24.62-35.462 54.947-35.462 30.325 0 54.946 15.89 54.946 35.462 0 19.57-24.62 35.46-54.947 35.46s-54.948-15.89-54.948-35.46z" fill="#008DC9"/><path d="M1144.365 577.88c0-13.914 4.98-25.21 11.11-25.21 6.132 0 11.11 11.296 11.11 25.21 0 13.916-4.978 25.213-11.11 25.213-6.13 0-11.11-11.297-11.11-25.212z" fill="#FEB931"/>
           <g id="robot-head" class="robot-head"><path d="M1165.73 497.545l16.24 23.93-67.09 42.304-63.67-49.142 23.075-26.494 47.432-15.81 44.014 25.21z" fill="#FFEF02" vector-effect="non-scaling-stroke"/><path d="M1114.88 563.78l6.837-44.442-47.432-31.194-23.075 26.494 63.67 49.14z" fill="#FEB931" vector-effect="non-scaling-stroke"/><path d="M1087.105 521.475l-14.102 4.608 1.282-30.247 12.82 25.64zM1089.24 524.04l1.157 15.81 25.37-13.767-26.526-2.044z" fill="#E51A24"/></g>
         </svg>
-        <div class="projects">
-          <a href="#" class="project project_nintendo">
-            <img src="~assets/img/logo-nintendo.png" alt="">
-          </a>
-          <a href="#" class="project project_mind">
-            <img src="~assets/img/logo-mind.png" alt="">
-          </a>
-          <a href="#" class="project project_cav">
-            <img src="~assets/img/logo-cav.png" alt="">
-          </a>
-          <a href="#" class="project project_telstra">
-            <img src="~assets/img/logo-telstra.png" alt="">
-          </a>
-        </div>
+        <HomeProjects></HomeProjects>
       </div>
     </div>
 
@@ -34,6 +21,7 @@
 
 <script>
 import { TweenLite } from 'gsap'
+import HomeProjects from '~/components/HomeProjects'
 
 export default {
   data () {
@@ -41,6 +29,9 @@ export default {
       flip: false,
       robotValModifier: 1
     }
+  },
+  components: {
+    HomeProjects
   },
   watch: {
     flip (value) {
@@ -135,37 +126,6 @@ export default {
 .flipped {
   transform-origin: 50% 50%;
   transform: scaleX(-1);
-}
-
-.projects {
-  display: flex;
-  flex-wrap: wrap;
-  margin-top: 200px;
-  position: relative;
-  z-index: 2;
-}
-
-.project {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 250px;
-  height: 150px;
-  flex: 1 1 50%;
-
-  &_nintendo {
-    background-color: $c-nintendo;
-  }
-  &_mind {
-    background-color: $c-mind;
-  }
-  &_cav {
-    background-color: $c-cav;
-  }
-  &_telstra {
-    background-color: $c-telstra;
-  }
-
 }
 
 </style>
