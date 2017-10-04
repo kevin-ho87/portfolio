df44e04066669a056f1f2f3b44cb3c60ef32530b
kevin-ho87 <31581926+kevin-ho87@users.noreply.github.com>
2017-10-04 21:41:50 +1100

    Projects
    Update images, screenshots, project data

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,14 +36,64 @@ module.exports = {
             mobile: 'mobile-nintendo-1.jpg'
           },
           {
-            desktop: 'desktop-nintendo-1.jpg',
-            tablet: 'tablet-nintendo-1.jpg',
-            mobile: 'mobile-nintendo-1.jpg'
+            desktop: 'desktop-nintendo-2.jpg',
+            tablet: 'tablet-nintendo-2.jpg',
+            mobile: 'mobile-nintendo-2.jpg'
           }
         ]
       },
       {
         id: 2,
+        path: 'mind',
+        title: 'Mind Australia',
+        description: 'Lorem ipsum dolor sit amet.',
+        url: 'https://www.nintendo.com.au/yours-to-play/',
+        logo: 'nintendo-logo.png',
+        color: '#A0148C',
+        tech: 'Greensock, jQuery, HTML5, CSS3',
+        year: '2017',
+        info: 'Front end. lorem.',
+        gifs: ['desktop-mind-1.jpg', 'desktop-mind-2.jpg'],
+        screenshots: [
+          {
+            desktop: 'desktop-mind-1.jpg',
+            tablet: 'tablet-mind-1.jpg',
+            mobile: 'mobile-mind-1.jpg'
+          },
+          {
+            desktop: 'desktop-mind-2.jpg',
+            tablet: 'tablet-mind-2.jpg',
+            mobile: 'mobile-mind-2.jpg'
+          }
+        ]
+      },
+      {
+        id: 3,
+        path: 'cav',
+        title: 'Party for your rights',
+        description: 'Lorem ipsum dolor sit amet.',
+        url: 'https://www.nintendo.com.au/yours-to-play/',
+        logo: 'nintendo-logo.png',
+        color: '#A0148C',
+        tech: 'Greensock, jQuery, HTML5, CSS3',
+        year: '2017',
+        info: 'Front end. lorem.',
+        gifs: ['desktop-cav-1.jpg', 'desktop-cav-2.jpg'],
+        screenshots: [
+          {
+            desktop: 'desktop-cav-1.jpg',
+            tablet: 'desktop-cav-1.jpg',
+            mobile: 'mobile-cav-1.jpg'
+          },
+          {
+            desktop: 'desktop-cav-2.jpg',
+            tablet: 'desktop-cav-2.jpg',
+            mobile: 'mobile-cav-2.jpg'
+          }
+        ]
+      },
+      {
+        id: 4,
         path: 'telstra',
         title: 'Telstra exchange',
         description: 'Lorem ipsum dolor sit amet.',
@@ -53,12 +103,17 @@ module.exports = {
         tech: 'Greensock, jQuery, HTML5, CSS3',
         year: '2017',
         info: 'Front end. lorem.',
-        gifs: ['desktop-nintendo-1.jpg', 'desktop-nintendo-1.jpg'],
+        gifs: ['desktop-telstra-1.jpg', 'desktop-telstra-2.jpg'],
         screenshots: [
           {
-            desktop: 'desktop-nintendo-1.jpg',
-            tablet: 'tablet-nintendo-1.jpg',
-            mobile: 'mobile-nintendo-1.jpg'
+            desktop: 'desktop-telstra-1.jpg',
+            tablet: 'tablet-telstra-1.jpg',
+            mobile: 'mobile-telstra-1.jpg'
+          },
+          {
+            desktop: 'desktop-telstra-2.jpg',
+            tablet: 'tablet-telstra-2.jpg',
+            mobile: 'mobile-telstra-2.jpg'
           }
         ]
       }
